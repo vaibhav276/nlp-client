e694842ceeb51e65452b4b5e1e4b47d0423e79d2
Vaibhav Pujari <276vaibhav+github@gmail.com>
2020-11-18 17:25:09 +0530

    Basic app without any styles

---
+++ public/js/NLPEngineMock.js
@@ -0,0 +1,68 @@
+class NLPEngineMock {
+    constructor(serverAddr) {
+	this.stepIter = 0;
+	this.allSteps = [
+	    'Extracting text',
+	    'Identifying sentences',
+	    'Running algorithms',
+	    'Done'
+	]
+    }
+
+    _create = url => {
+    	return 'k3kjk'; // reqId
+    }
+    
+    _steps = reqId => {
+    	let res = [];
+    	for (let i = 0; i <= this.stepIter; i++) {
+    	    res.push(this.allSteps[i]);
+    	}
+    	if (this.stepIter < this.allSteps.length - 1 &&
+	    Math.random() < 0.3) this.stepIter++;
+    	return res;
+    }
+
+    _nlpResults = reqId => {
+	const excerpt = "The economic situation is apparently so grim that some experts fear we may be in for a stretch as bad as the mid seventies. When Microsoft and Apple were founded. As those examples suggest, a recession may not be such a bad time to start a startup. I'm not claiming it's a particularly good time either. The truth is more boring: the state of the economy doesn't matter much either way. If we've learned one thing from funding so many startups, it's that they succeed or fail based on the qualities of the founders. The economy has some effect, certainly, but as a predictor of success it's rounding error compared to the founders.";
+	
+	const score = Math.random(); // [0, 1)
+
+	const components = [
+	    {name: 'clickbait', desc: 'Article headlines which at the expense of being informative, are designed to entice readers into clicking the accompanying link.', value: Math.random()},
+	    {name: 'politicalBias', desc: 'Strongly biased political language.', value: Math.random()},
+	    {name: 'toxicity', desc: 'Demeaning and abusive language in general.', value: Math.random()},
+	    {name: 'obscenity', desc: 'Obscene or profane language.', value: Math.random()},
+	    {name: 'racism', desc: 'Demeaning and abusive language targeted towards a particular ethnicity, usually with stereotypes.', value: Math.random()},
+	    {name: 'sexism', desc: 'Demeaning and abusive language targeted towards a particular gender, usually with stereotypes.', value: Math.random()},
+	    {name: 'insult', desc: 'Scornful remarks directed towards an individual.', value: Math.random()},
+	    {name: 'threat', desc: 'Expressing a wish or intention for pain, injury, or violence against an individual or group.', value: Math.random()}
+	];
+
+	return {
+	    excerpt: excerpt,
+	    score: score,
+	    components: components
+	}
+    }
+
+    create = url => {
+	return this._create(url);
+    }
+    
+    fetch = reqId => {
+	const ss = this._steps(reqId);
+	let done = false;
+	let nlpResults = null;
+	if (ss.length === this.allSteps.length) done = true;
+	if (done === true) {
+	    nlpResults = this._nlpResults(reqId);
+	}
+
+	return {
+	    steps: ss,
+	    done: done,
+	    results: nlpResults
+	}
+    }
+}
---
+++ public/js/index.js
@@ -0,0 +1,111 @@
+// Global variables
+let poller = null;
+let reqId = null;
+let nlpEngine = null;
+const pollDuration = 1000; // milliseconds
+const btnStartStop = d3.select('#btnStartStop');
+const txtInputUrl = d3.select('#txtInputUrl');
+
+// Event Handlers
+const startStopHandler = () => {
+    if (reqId === null) {
+	resetDOM();
+	nlpEngine = new NLPEngineMock('localhost:3000');
+	reqId = createRequest();
+	startPolling();
+    }
+    else {
+	stopPolling();
+	reqId = null;
+	nlpEngine = null;
+    }
+}
+
+// Utility functions
+const createRequest = () => {
+    const inputUrl = txtInputUrl.property('value');
+    reqId = nlpEngine.create(inputUrl);
+}
+
+const startPolling = () => {
+    btnStartStop.text('Stop');
+    poller = setInterval(pollHandler, pollDuration);
+}
+
+const stopPolling = () => {
+    btnStartStop.text('Start');
+    clearInterval(poller);
+    poller = null; // TODO: execute after async call to clearInterval above returns
+    reqId = null;
+}
+
+const pollHandler = () => {
+    // Poll server and update DOM
+    const data = nlpEngine.fetch(reqId);
+    updateDOM(data);
+    if (data.done === true) stopPolling();
+}
+
+const resetDOM = () => {
+    d3.select('#ulSteps').selectAll('li').remove();
+    const divExcerpt = d3.select('#divExcerpt');
+    divExcerpt.select('a')
+	.attr('href', null)
+	.text('');
+    divExcerpt.select('p').text('');
+
+    d3.select('#divScore').select('p').text('');
+
+    d3.select('#divComponents').select('table').remove();
+}
+
+const updateDOM = data => {
+    // Update DOM
+    d3.select('#ulSteps').selectAll('li')
+	.data(data.steps)
+	.join('li')
+	.text(d => d);
+
+    if (data.done === true) {
+	d3.select('#ulSteps').selectAll('li')
+	    .transition()
+	    .ease(d3.easeLinear)
+	    .duration(1000)
+	    .remove();
+
+	const divExcerpt = d3.select('#divExcerpt');
+	divExcerpt.select('a')
+	    .attr('href', txtInputUrl.property('value'))
+	    .attr('target', '_blank')
+	    .text('View original page');
+	divExcerpt.select('p').text(data.results.excerpt);
+
+	d3.select('#divScore').select('p').text(scoreString(data.results.score));
+
+	d3.select('#divComponents').append('table').selectAll('tr')
+	    .data(data.results.components)
+	    .join('tr')
+	    .selectAll('td')
+	    .data(d => [camel2title(d.name), floatToPrecentageString(d.value) + ' likely', d.desc])
+	    .enter().append('td')
+	    .text(d => d);
+    }
+}
+
+const camel2title = camelCase => camelCase
+  .replace(/([A-Z])/g, match => ` ${match}`)
+      .replace(/^./, match => match.toUpperCase());
+
+const floatToPrecentageString = n => {
+    const rn = Number(Math.round((n * 100)+'e'+2)+'e-'+2);
+    return rn + '%';
+}
+
+const scoreString = n => {
+    return 'Overall Score: '
+	+ n
+	+ ' (<0.3 : Content may be harmful, 0.3 - 0.7: Content is questionble, >0.7: Content looks good)';
+}
+
+// Event bindings
+btnStartStop.on('click', startStopHandler);
